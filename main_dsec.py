094b025c6be47391626da7e0ad06d6fe83fbdf5b
odvorak <54942164+odvorak@users.noreply.github.com>
2024-12-10 22:22:39 +0100

    corrected out save for test_wo_reset

--- main_dsec.py
+++ main_dsec.py
@@ -166,6 +166,10 @@ def validate(test_loader, model, mode, visualize, save_visualization_dir, n_spli
                     pred_list.append(pred_flows[1])
                     gt_list.append(gt_flows[1])
                     mask_list.append(gt_flow_masks[1])
+                elif model.module.__class__.__name__ in ['NonSpikingEVFlowNet']:
+                    pred_list.append(pred_flows[0])
+                    gt_list.append(gt_flows[0])
+                    mask_list.append(gt_flow_masks[0])
                 else:
                     pred_list.append(pred_flows[3])
                     gt_list.append(gt_flows[3])
