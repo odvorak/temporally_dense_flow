0795906cba1fc9f2d3cbfefd39ddee244ad50f8b
odvorak <54942164+odvorak@users.noreply.github.com>
2025-02-24 20:31:36 +0100

    loss to corr

--- main_dsec.py
+++ main_dsec.py
@@ -91,7 +91,7 @@ def train(train_loader, model, optim, epoch, log_file, no_grad_split, grad_scala
             kernel_size = 50
             #
             # # Create a mean kernel (normalized)
-            mean_kernel = torch.ones((1, 1, kernel_size, kernel_size), device='cuda') / (kernel_size ** 2)
+            mean_kernel = torch.ones((1, kernel_size, kernel_size), device='cuda') / (kernel_size ** 2)
             #
             # # Apply the mean filter to smooth the entire field
             unsig_u_smooth = torch.nn.functional.conv2d(sig_u, mean_kernel, padding=kernel_size // 2)
